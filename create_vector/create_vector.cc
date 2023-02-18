80212ce66ec07b102c44c4cd28fc67da4aed0020
paris_ngc <paris@ngc.com>
2023-02-18 17:12:43 -0600

    Completed create_vector

--- create_vector/create_vector.cc
+++ create_vector/create_vector.cc
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int main ()
-{
-  // vector<int> v{10,5};
-  // vector<int> v = {10,5};
-  // vector<int> v(10,5);
-  // vector<int> v = (10,5);
+int main (){
+  vector<int> v{10,5};
+ // vector<int> v = {10,5};
+ // vector<int> v(10,5);
+ // vector<int> v = (10,5);
 
   for (auto& elem : v) {
     cout << elem << " ";
   }
+
   cout << endl;
 
   return 0;
