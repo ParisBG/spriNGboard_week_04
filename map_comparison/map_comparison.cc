6db7c03302c0c04e209461a0a1fbbaf0ebae5827
paris_ngc <paris@ngc.com>
2023-02-18 19:23:03 -0600

    Completed map_comparison

--- map_comparison/map_comparison.cc
+++ map_comparison/map_comparison.cc
@@ -1,68 +1,13 @@
 #include <iostream>
 #include <vector>
-
-/////////////////////////////////////////
-// INCLUDE NECESSARY HEADER FILES HERE //
-/////////////////////////////////////////
-
+#include <map>
+#include <unordered_map>
 using namespace std;
 
-void func_map (vector<pair<int,string>>& items)
-{
-  cout << endl << "=== BEGIN func_map ===" << endl;
+void func_map (vector<pair<int,string>>&);
+void func_unordered_map (vector<pair<int,string>>&);
 
-  ////////////////////////////////////////////////
-  // CREATE AN EMPTY map MAPPING int TO string. //
-  ////////////////////////////////////////////////
-
-
-  ////////////////////////////////////////////
-  // USING A RANGE-BASED for(...) LOOP,     //
-  // INSERT EACH ITEM IN items INTO THE MAP //
-  ////////////////////////////////////////////
-
-
-  //////////////////////////////////////////////
-  // USING A RANGE-BASED for(...) LOOP,       //
-  // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP //
-  //////////////////////////////////////////////
-
-
-  cout << endl << "=== END func_map ===" << endl;
-}
-
-void func_unordered_map (vector<pair<int,string>>& items)
-{
-  cout << endl << "=== BEGIN func_unordered_map ===" << endl;
-
-  //////////////////////////////////////////////////////////
-  // CREATE AN EMPTY unordered_map MAPPING int TO string. //
-  //////////////////////////////////////////////////////////
-
-
-  /////////////////////////////////////////////
-  // USING A RANGE-BASED for(...) LOOP,      //
-  // INSERT EACH ITEM IN items INTO THE MAP. //
-  // AFTER EACH INSERTION, PRINT OUT:        //
-  //   o  THE SIZE OF THE MAP                //
-  //   o  THE NUMBER OF BUCKETS              //
-  //   o  THE LOAD FACTOR                    //
-  // IN THE FOLLOWING FORMAT:                //
-  //   o  "[N,B,LF] = [3,10,0.4432]"         //
-  /////////////////////////////////////////////
-
-
-  ///////////////////////////////////////////////
-  // USING A RANGE-BASED for(...) LOOP,        //
-  // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP. //
-  ///////////////////////////////////////////////
-
-
-  cout << endl << "=== END func_unordered_map ===" << endl;
-}
-
-int main ()
-{
+int main (){
   vector<pair<int,string>> items = {
     {3, "three"},
     {13, "thirteen"},
@@ -86,3 +31,41 @@ int main ()
   return 0;
 }
 
+void func_map (vector<pair<int,string>>& items){
+  cout << endl << "=== BEGIN func_map ===" << endl;
+
+  map<int,string> maptastic;
+  
+  for (const auto& pair : items){
+    maptastic.insert(pair);
+  }
+
+  for (const auto& pair : maptastic){
+    cout << "Key: " << pair.first << endl;
+    cout << "Val: " << pair.second << endl;
+  }
+
+  cout << endl << "=== END func_map ===" << endl;
+}
+
+
+void func_unordered_map (vector<pair<int,string>>& items){
+  cout << endl << "=== BEGIN func_unordered_map ===" << endl;
+
+  unordered_map<int,string> sloppy;
+  
+  for (const auto& pair : items){
+    sloppy.insert(pair); 
+    cout << "Inserted: " << pair.first << endl;
+    cout << "[N,B,LF] = [" << sloppy.size() << "," << sloppy.bucket_count() << "," << sloppy.load_factor() << "]" << endl;
+  }
+  
+  cout << endl;
+
+  for (const auto& pair : sloppy){
+    cout << "Key: " << pair.first << endl;
+    cout << "Val: " << pair.second << endl;
+  }
+
+  cout << endl << "=== END func_unordered_map ===" << endl;
+}
